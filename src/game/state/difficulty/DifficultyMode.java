a328bb4f799818f6015d36126cf30f43f4b93b4a
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-13 16:40:58 +0900

    [수정] DifficultyMode 구조 변경 - 기존 /state에서 /state/difficulty로 이동

--- src/game/state/difficulty/DifficultyMode.java
+++ src/game/state/difficulty/DifficultyMode.java
@@ -1,2 +1,27 @@
-package game.state.difficulty;public enum DifficultyMode {
+package game.state.difficulty;
+
+import java.util.Arrays;
+
+public enum DifficultyMode {
+    EASY(1),
+    MEDIUM(2),
+    HARD(3);
+
+    private final int option;
+
+    DifficultyMode(int option) {
+        this.option = option;
+    }
+
+    public int getOption() {
+        return option;
+    }
+    public static DifficultyMode getDifficultyMode(int option){
+       return Arrays.stream(values())
+               .filter(v -> v.option == option)
+               .findFirst()
+               .orElseThrow(
+                       IllegalArgumentException::new
+               );
+    }
 }

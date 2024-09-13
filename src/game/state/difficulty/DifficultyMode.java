5ec49ae8521234256b8415e0cc7641804f16e5c7
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-13 22:21:23 +0900

    [수정] DifficultyMode 수정 - 난이도 선택에 대한 옵션과 선택한 난이도에 대한 숫자 길이를 갖도록 수정 - 현재 난이도의 숫자 길이 구하는 메서드 추가

--- src/game/state/difficulty/DifficultyMode.java
+++ src/game/state/difficulty/DifficultyMode.java
@@ -3,25 +3,38 @@ package game.state.difficulty;
 import java.util.Arrays;
 
 public enum DifficultyMode {
-    EASY(1),
-    MEDIUM(2),
-    HARD(3);
+    EASY(1, 3),
+    MEDIUM(2, 4),
+    HARD(3, 5);
 
     private final int option;
+    private final int len;
 
-    DifficultyMode(int option) {
+    DifficultyMode(int option, int len) {
         this.option = option;
+        this.len = len;
     }
 
     public int getOption() {
         return option;
     }
-    public static DifficultyMode getDifficultyMode(int option){
+
+    public int getLen() {
+        return len;
+    }
+
+    public static DifficultyMode findByOption(int option){
        return Arrays.stream(values())
                .filter(v -> v.option == option)
                .findFirst()
                .orElseThrow(
                        IllegalArgumentException::new
                );
+    }
+    public static DifficultyMode findByLen(int len){
+        return Arrays.stream(values())
+                .filter(v -> v.len == len)
+                .findFirst()
+                .get();
     }
 }

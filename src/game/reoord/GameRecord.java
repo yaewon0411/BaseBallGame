5e3136549616db664ca723ea9d74781059d1bfe8
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-13 22:06:23 +0900

    [수정] GameRecord 클래스 게임 횟수 필드 삭제 - User 클래스에서 게임 횟수 구하도록 수정함에 따라 gameNumber 필드 삭제

--- src/game/reoord/GameRecord.java
+++ src/game/reoord/GameRecord.java
@@ -8,17 +8,12 @@ import java.util.Map;
 
 public class GameRecord {
 
-    private int gameNumber; //몇 번째 게임인지
     private int attemptCnt; //게임 시도 횟수
     private final DifficultyMode difficultyMode;
 
     public GameRecord(int gameNumber,  DifficultyMode difficultyMode) {
-        this.gameNumber = gameNumber;
         this.attemptCnt = 0;
         this.difficultyMode = difficultyMode;
-    }
-    public void increaseGameNumber() {
-        this.gameNumber++;
     }
 
     public void increaseAttemptCnt() {
@@ -27,10 +22,6 @@ public class GameRecord {
 
     public DifficultyMode getDifficultyMode() {
         return difficultyMode;
-    }
-
-    public int getGameNumber() {
-        return gameNumber;
     }
 
     public int getAttemptCnt() {

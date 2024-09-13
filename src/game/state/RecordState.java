ade86f4fd3e7317dc408217e01c78db96f042639
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-13 22:08:22 +0900

    [생성] RecordState 기능 추가 - n번째 게임 별 시도 횟수 출력 - 출력 후 MenuState으로 상태 전환

--- src/game/state/RecordState.java
+++ src/game/state/RecordState.java
@@ -1,6 +1,8 @@
 package game.state;
 
 import game.BaseballGame;
+import game.reoord.GameRecord;
+import user.User;
 
 /*
 * < 게임 기록 보기 >
@@ -11,7 +13,20 @@ import game.BaseballGame;
 public class RecordState implements GameState{
 
     @Override
-    public void handle(BaseballGame baseballGame) {
+    public void handle(BaseballGame baseballGame, User user) {
+        System.out.println(user.getUsername()+"님의 게임 기록은 다음과 같습니다");
 
+        if(user.getGameRecordList().isEmpty())
+            System.out.println("======게임을 진행한 이력이 없습니다=======");
+
+        else {
+            int i = 1;
+            for (GameRecord record : user.getGameRecordList()) {
+                System.out.println(i++ + "번째 게임 [" + record.getDifficultyMode() + "] 시도 횟수 : " + record.getAttemptCnt());
+            }
+        }
+
+        //다시 메뉴 옵션 출력
+        baseballGame.nextStep(MenuState.getInstance());
     }
 }

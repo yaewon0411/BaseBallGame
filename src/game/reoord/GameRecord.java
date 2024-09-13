f3caabafb863fd43e6a73f4135b88375ac54284d
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-13 22:03:51 +0900

    [생성] GameRecord 기능 추가 - 현재 게임 모드도 저장하도록 DifficultyMode 필드 추가 - 게임 시도 횟수 증가 메서드 추가 - 현재 모드 반환 메서드 추가

--- src/game/reoord/GameRecord.java
+++ src/game/reoord/GameRecord.java
@@ -1,28 +1,39 @@
 package game.reoord;
 
+import game.state.difficulty.DifficultyMode;
 import user.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class GameRecord {
-    private User user;
+
     private int gameNumber; //몇 번째 게임인지
     private int attemptCnt; //게임 시도 횟수
+    private final DifficultyMode difficultyMode;
 
-    public GameRecord(User user) {
-        this.gameNumber = 0;
+    public GameRecord(int gameNumber,  DifficultyMode difficultyMode) {
+        this.gameNumber = gameNumber;
         this.attemptCnt = 0;
-        this.user = user;
+        this.difficultyMode = difficultyMode;
     }
     public void increaseGameNumber() {
         this.gameNumber++;
     }
 
-    public void increaseAttemptCnt(int attemptCnt) {
+    public void increaseAttemptCnt() {
         this.attemptCnt++;
     }
-    public void addToGameRecordList(){
-        this.user.getGameRecordList().add(this);
+
+    public DifficultyMode getDifficultyMode() {
+        return difficultyMode;
+    }
+
+    public int getGameNumber() {
+        return gameNumber;
+    }
+
+    public int getAttemptCnt() {
+        return attemptCnt;
     }
 }

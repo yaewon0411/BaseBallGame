4e77d5abef27e27332133fc08e9c5063e5a3ba77
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-14 18:37:24 +0900

    [수정] DifficultyState의 validateAnswer 로직 분리 - 입력 값 검증은 validateInput에서 수행하도록 분리

--- src/game/state/difficulty/DifficultyStatus.java
+++ src/game/state/difficulty/DifficultyStatus.java
@@ -1,5 +1,8 @@
 package game.state.difficulty;
 
+import ex.InvalidInputException;
+import util.CustomDesign;
+
 import java.util.*;
 
 public class DifficultyStatus {
@@ -43,27 +46,18 @@ public class DifficultyStatus {
         while(answers.size() < LEN){
             answers.add(random.nextLong(1L, 10L));
         }
-        System.out.println("랜덤 넘버 생성 완료 ✨ : ");
-        for (Long answer : answers) {
-            System.out.print(answer);
-        }
-        System.out.println();
+        System.out.println("랜덤 넘버 생성 완료 ✨");
     }
 
     /**
      * 입력한 숫자 유효성 검증과
-     * 정답 판별 기능을 수행한 후
-     * 정답과 비교한 상태를 출력
-     *
      *
      * @param input 사용자가 입력한 숫자
-     * @return 정답이라면 true, 아니라면 false
      */
-    public boolean validateAnswer(String input){
+    public Set<Long> validateInput(String input) throws InvalidInputException {
 
         if (input.length() != LEN) {
-            System.out.println(LEN + " 자리수만큼 입력해야 합니다");
-            return false;
+            throw new InvalidInputException(LEN+" 자리수만큼 입력해야 합니다");
         }
 
         //String -> Long 파싱 및 길이 검증, 숫자 검증
@@ -71,20 +65,32 @@ public class DifficultyStatus {
         for (char c : input.toCharArray()) {
             // 0 입력 검증
             if(c == '0'){
-                System.out.println("0은 입력하시면 안됩니다!!!!");
-                return false;
+                throw new InvalidInputException("1부터 9까지의 정수만 입력해야 합니다.");
             }
             try{
                 long num = Long.parseLong(String.valueOf(c));
                 if (!inputSet.add(num)) {
-                    System.out.println("숫자 중복해서 입력하면 안됨");
-                    return false;
+                    throw new InvalidInputException("중복되지 않는 숫자를 입력해야 합니다.");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("숫자만 입력해야 함");
-                return false;
+                throw new InvalidInputException("유효하지 않은 입력입니다. 1부터 9까지의 정수만 입력해야 합니다.");
             }
         }
+
+        return inputSet;
+    }
+
+    public boolean validateAnswer(String input){
+
+        Set<Long> inputSet;
+        //입력 값 검증
+        try {
+             inputSet = validateInput(input);
+        }catch(Exception e){
+            CustomDesign.printExceptionMessage(e.getMessage());
+            return false;
+        }
+
         //정답과 비교
         Iterator<Long> inputIter = inputSet.iterator();
         Iterator<Long> answerIter = answers.iterator();
@@ -100,11 +106,8 @@ public class DifficultyStatus {
 
         outCnt = LEN - strikeCnt - ballCnt;
 
-
-        //스트라이크는 몇이고 볼은 몇이고 아웃은 몇입니다. 어쩌구저쩌구 출력
-        System.out.println("스트라이크 개수 : " + strikeCnt);
-        System.out.println("볼 개수 : "+ballCnt);
-        System.out.println("아웃 개수 : "+outCnt);
+        //결과 메시지 출력
+        CustomDesign.printResult(strikeCnt, ballCnt, outCnt);
 
         return strikeCnt == LEN;
     }

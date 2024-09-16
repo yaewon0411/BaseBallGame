92902ecfa3c0668a54967a068cf14789a7c51d06
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-16 13:40:24 +0900

    [수정] TriConsumer를 사용하도록 변경 - handle() 시그니처 변경에 따라 3개의 인자를 받기 위해 기존 BiConsumer -> TriConsumer로 변경

--- src/game/state/menu/MenuState.java
+++ src/game/state/menu/MenuState.java
@@ -1,31 +1,35 @@
-package game.state;
+package game.state.menu;
 
 import game.BaseballGame;
+import game.state.ExitState;
+import game.state.GameState;
+import game.state.RecordState;
+import game.state.RunState;
 import game.state.difficulty.DifficultyMode;
 import user.User;
 import util.CustomDesign;
 
-import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
 
 public class MenuState implements GameState {
     private static MenuState menuState;
-    private final Scanner sc;
-    private final Map<Integer, BiConsumer<BaseballGame, User>> optionStates;
+    private final Map<Integer, TriConsumer<BaseballGame, User, Scanner>> optionStates;
+    private MenuStatus currentStatus;
+
+    private static final int MIN_OPTION = 1;
+    private static final int MAX_OPTION = 3;
 
 
     private MenuState(){
-        sc = new Scanner(System.in);
-        optionStates = new HashMap<>();
+        this.optionStates = new HashMap<>();
         optionStates.put(1, this::validateDifficultyModeInput);
-        optionStates.put(2, (game, user) -> game.nextStep(new RecordState()));
-        optionStates.put(3, (game, user) -> game.nextStep(new ExitState()));
+        optionStates.put(2, (game, user, sc) -> game.nextStep(new RecordState()));
+        optionStates.put(3, (game, user, sc) -> game.nextStep(new ExitState()));
+        currentStatus = MenuStatus.MAIN_MENU;
     }
     public static synchronized MenuState getInstance(){
         if(menuState ==null){
@@ -35,96 +39,78 @@ public class MenuState implements GameState {
     }
 
     /**
-     * 옵션 입력 검증 및 상태 변경을 관리합니다
-     * 유효한 입력에 대해 야구 숫자 게임 상태를 변환합니다
+     * 사용자 입력을 처리하고 게임 상태를 변경합니다.
+     * 이 메서드는 메인 메뉴를 표시하고, 사용자의 선택에 따라 적절한 동작을 수행합니다.
      *
-     * @param baseballGame 야구 숫자 게임
+     * @param baseballGame 현재의 야구 게임 인스턴스
+     * @param user 현재 사용자
      */
-    public void handle(BaseballGame baseballGame, User user){
-        int option = validateMainMenuInput();
-        optionStates.getOrDefault(option, (game, u) -> CustomDesign.printExceptionMessage("유효한 옵션 번호를 입력해주세요"))
-                        .accept(baseballGame, user);
+    public void handle(BaseballGame baseballGame, User user, Scanner sc){
+        resetToMainMenu();
+        int option = validateMainMenuInput(sc);
+        optionStates.getOrDefault(option, (game, u, s) -> CustomDesign.printExceptionMessage("유효한 옵션 번호를 입력해주세요"))
+                        .accept(baseballGame, user, sc);
     }
 
     /**
-     * 사용자 입력 값 유효성을 검증합니다
-     * @return 유효한 옵션 값
+     * 메뉴 상태를 메인 메뉴로 재설정합니다.
      */
-    public int validMainMenuInput(){
-        int option = 0;
-        while(true) {
-            CustomDesign.printMainMenu();
-            try {
-                //옵션 입력
-                option = sc.nextInt();
-
-                //유효한 옵션이 아닌 경우
-                if(option != 1 && option!=2 && option!=3) throw new InputMismatchException();
-
-               break;
-
-            } catch (InputMismatchException e) { //정수로 변환할 수 없는 경우
-                CustomDesign.printExceptionMessage("유효한 옵션 번호를 입력해주세요");
-                continue;
-            }
-        }
-        return option;
+    private void resetToMainMenu() {
+        currentStatus = MenuStatus.MAIN_MENU;
     }
 
-    public void validDifficultyModeInput(BaseballGame baseballGame, User user){
-        DifficultyMode difficultyMode;
-        while(true) {
-            CustomDesign.printDifficultyMenu();
-            try {
-                //옵션 입력
-                int option = sc.nextInt();
-
-                //유효한 옵션인지 확인
-                difficultyMode = DifficultyMode.findByOption(option);
-
+    /**
+     * 현재 메뉴 상태에 따라 적절한 메뉴 옵션을 출력합니다.
+     */
+    private void printAvailableMenuOptions() {
+        switch (currentStatus) {
+            case MAIN_MENU:
+                CustomDesign.printMainMenu();
                 break;
-
-            } catch (InputMismatchException | IllegalArgumentException e) { //정수로 변환할 수 없는 경우
-                CustomDesign.printExceptionMessage("유효한 옵션 번호를 입력해주세요");
-                continue;
-            }
-        }
-        System.out.println("난이도 선택 완료");
-        //난이도에 따른 게임 로직 실행
-        baseballGame.nextStep(new RunState(difficultyMode));
-    }
-
-
-    private boolean isMainMenuSelected = false;
-
-    private void printAvailableMenuOptions(){ //현재 상황에서 선택 가능한 메뉴 출력
-        if(!isMainMenuSelected){
-            CustomDesign.printMainMenu();
-        }else{
-            CustomDesign.printDifficultyMenu();
+            case DIFFICULTY_SELECTION:
+                CustomDesign.printDifficultyMenu();
+                break;
         }
     }
 
-    private static final int MIN_OPTION = 1;
-    private static final int MAX_OPTION = 3;
 
-    private int validateMainMenuInput(){
+    /**
+     * 메인 메뉴 입력을 검증합니다.
+     *
+     * @return 유효한 메인 메뉴 옵션 번호
+     */
+    private int validateMainMenuInput(Scanner sc){
         return validateInput(
                 option -> {
                     if (option < MIN_OPTION || option > MAX_OPTION) throw new IllegalArgumentException("유효한 옵션 번호를 입력해주세요");
                     return option;
                 }
+                ,sc
         );
     }
 
-    private void validateDifficultyModeInput(BaseballGame baseballGame, User user){
-        DifficultyMode difficultyMode = validateInput(DifficultyMode::findByOption);
+    /**
+     * 난이도 모드 입력을 검증하고 게임 상태를 변경합니다.
+     *
+     * @param baseballGame 현재의 야구 게임 인스턴스
+     * @param user 현재 사용자
+     */
+    private void validateDifficultyModeInput(BaseballGame baseballGame, User user, Scanner sc){
+        currentStatus = MenuStatus.DIFFICULTY_SELECTION;
+        DifficultyMode difficultyMode = validateInput(DifficultyMode::findByOption, sc);
         baseballGame.nextStep(new RunState(difficultyMode));
-        isMainMenuSelected = false;
     }
 
 
-    private <T> T validateInput(Function<Integer, T> validator){
+    /**
+     * 사용자 입력을 검증합니다.
+     * 다양한 타입의 입력 검증에 사용될 수 있습니다.
+     *
+     * @param <T> 반환될 값의 타입
+     * @param validator 입력을 검증하고 변환하는 함수
+     * @return 검증된 입력 값
+     */
+    private <T> T validateInput(Function<Integer, T> validator, Scanner sc){
         while(true){
             printAvailableMenuOptions();
             try{
@@ -136,8 +122,5 @@ public class MenuState implements GameState {
             }
         }
     }
-
-
-
 
 }

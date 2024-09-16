2c73ea9706e09fbb24084f7bd94e701fd27d4878
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-16 13:37:53 +0900

    [수정] 매개변수로 Scanner 추가 - Scanner 인스턴스가 여러 상태마다 사용되므로 Scanner 객체를 주입받아 사용하도록 변경

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -3,6 +3,18 @@ package game.state;
 import game.BaseballGame;
 import user.User;
 
+import java.util.Scanner;
+/**
+ * GameState 인터페이스는 게임의 각 상태를 표현합니다
+ * 각 상태는 이 인터페이스를 구현하여 고유 동작을 정의합니다
+ */
 public interface GameState {
-    void handle(BaseballGame baseballGame);
+    /**
+     * 현재 게임 상태에 대한 처리를 수행합니다
+     *
+     * @param baseballGame 현재의 야구 게임 인스턴스
+     * @param user 현재 사용자
+     * @param sc 사용자 입력을 받기 위한 Scanner 객체
+     */
+    void handle(BaseballGame baseballGame, User user, Scanner sc);
 }

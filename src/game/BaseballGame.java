1133c848f2ce72cf5c36b7b65014ec34f92fe272
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-16 16:38:47 +0900

    [수정] User 관리 위한 기능 추가 - 기록 삭제 시 유저 객체에서 GameRecord 삭제한 후 전체 userList 삭제 - 현재 유저를 currentUser로 관리

--- src/game/BaseballGame.java
+++ src/game/BaseballGame.java
@@ -2,25 +2,61 @@ package game;
 
 
 import game.state.GameState;
+import game.state.StartState;
 import user.User;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BaseballGame {
     private GameState gameState;
+    private final Scanner sc;
+    private boolean isRunning;
+    private User currentUser;
+
+    public BaseballGame() {
+        sc = new Scanner(System.in);
+        isRunning = true;
+    }
+
+    public void exit() {
+        isRunning = false;
+    }
+
     private List<User> userList = new ArrayList<>();
 
+    public List<User> getUserList(){
+        return this.userList;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+    public void clearHistory(){
+        this.userList.forEach(User::clearGameRecords);
+        this.userList.clear();
+    }
 
     public void nextStep(GameState gameState){
         this.gameState = gameState;
     }
 
-    public void addToUserList(User user){
 
+    public void setCurrentUser(User user){
+        this.currentUser = user;
     }
 
-    public List<User> getUserList() {
-        return userList;
+    public void start(){
+        gameState = StartState.getInstance();
+        while(isRunning) {
+            gameState.handle(this, sc);
+        }
     }
+
+    public static void main(String[] args) {
+        BaseballGame game = new BaseballGame();
+        game.start();
+    }
+
+
+
 }

739ebfd03f3c69280ae76d70ca95f2699ed46171
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-16 16:40:13 +0900

    [수정] 게임 종료 상태 관리 클래스 수정 - 싱글톤으로 변경 - 게임 종료 시 모든 기록 삭제

--- src/game/state/ExitState.java
+++ src/game/state/ExitState.java
@@ -1,10 +1,30 @@
 package game.state;
 
 import game.BaseballGame;
+import user.User;
+import util.CustomDesign;
+
+import java.util.Scanner;
 
 public class ExitState implements GameState{
-    @Override
-    public void handle(BaseballGame baseballGame) {
 
+    public static ExitState exitState;
+    private ExitState(){}
+
+    public static ExitState getInstance(){
+        if(exitState==null){
+            exitState = new ExitState();
+        }
+        return exitState;
+    }
+    @Override
+    public void handle(BaseballGame baseballGame, Scanner sc) {
+        CustomDesign.printExitMessage();
+        //기록 다 지우기
+        baseballGame.clearHistory();
+        //스캐너 닫기
+        sc.close();
+        //종료
+        baseballGame.exit();
     }
 }

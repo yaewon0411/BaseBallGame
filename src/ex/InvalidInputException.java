8926d48c536fa6f8077e88c9fe2a187391c7e0e8
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-19 20:38:50 +0900

    [수정] javadoc 수정

--- src/ex/InvalidInputException.java
+++ src/ex/InvalidInputException.java
@@ -1,7 +1,7 @@
 package ex;
 
 /**
- * 입력 값 예외에 대한 에러
+ * 게임 중 발생할 수 있는 입력 값 예외를 처리하는 클래스
  */
 public class InvalidInputException extends RuntimeException {
     public InvalidInputException(String msg) {

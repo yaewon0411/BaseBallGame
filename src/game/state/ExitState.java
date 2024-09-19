72c3e07131edb24cd6b2c150ffcae8e14c81b227
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-19 19:53:41 +0900

    [생성] ExitState javadoc 추가

--- src/game/state/ExitState.java
+++ src/game/state/ExitState.java
@@ -6,6 +6,9 @@ import util.CustomDesign;
 
 import java.util.Scanner;
 
+/**
+ * 게임 종료 상태를 관리하는 클래스입니다
+ */
 public class ExitState implements GameState{
 
     public static ExitState exitState;
@@ -17,6 +20,14 @@ public class ExitState implements GameState{
         }
         return exitState;
     }
+
+    /**
+     * 게임 종료 상태를 처리합니다
+     * 종료 메시지를 출력하고,
+     * 야구 게임 인스턴스에 저장된 모든 기록을 삭제한 후 종료 상태로 전환합니다
+     * @param baseballGame 현재의 야구 게임 인스턴스
+     * @param sc 사용자 입력을 받기 위한 Scanner 객체
+     */
     @Override
     public void handle(BaseballGame baseballGame, Scanner sc) {
         CustomDesign.printExitMessage();

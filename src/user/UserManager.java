a2b138b4738b39494c19930dd68e2b202c467083
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-21 16:52:01 +0900

    [생성] 유저 관련 기능 추가 - 전체 유저 리스트 관리 - username을 통해 기존 유저 찾기 - 전체 유저 리스트 초기화

--- src/user/UserManager.java
+++ src/user/UserManager.java
@@ -1,2 +1,42 @@
-package user;public class UserManager {
+package user;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserManager {
+    public static UserManager userManager;
+
+    private List<User> userList;
+    private UserManager(){
+        userList = new ArrayList<>();
+    }
+
+    public static synchronized UserManager getInstance(){
+        if(userManager == null){
+            userManager = new UserManager();
+        }
+        return userManager;
+    }
+
+    public List<User> getUserList(){
+        return this.userList;
+    }
+
+    public void clearAllUser(){
+        this.userList.forEach(User::clearGameRecords);
+        this.userList.clear();
+    }
+
+    public Optional<User> findByUsername(String username){
+        return userList.stream()
+                .filter(user -> user.getUsername().equals(username))
+                .findFirst();
+    }
+
+    public void addUser(User user){
+        this.userList.add(user);
+    }
+
+
 }

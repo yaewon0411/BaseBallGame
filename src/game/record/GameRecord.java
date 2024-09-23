2290cbdbedcd4f85aaeea8130a31ab7285133c1a
ㅇi쀼z1 <yaewon0411@naver.com>
2024-09-23 15:53:14 +0900

    [생성] 게임 성공 여부 및 게임 성공 일자 필드 추가 - setter & getter 생성

--- src/game/record/GameRecord.java
+++ src/game/record/GameRecord.java
@@ -2,6 +2,9 @@ package game.record;
 
 import game.difficulty.DifficultyMode;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * 사용자의 게임 기록을 관리하는 클래스입니다
  * 게임 시도 횟수와 게임 난이도를 저장합니다
@@ -12,10 +15,30 @@ public class GameRecord {
 
     private int attemptCnt; //게임 시도 횟수
     private final DifficultyMode difficultyMode; //게임 난이도
+    private boolean isFinished; //게임 성공 여부
+
+    private LocalDateTime finishedDate; //게임 성공 일자
 
     public GameRecord(int gameNumber,  DifficultyMode difficultyMode) {
         this.attemptCnt = 0;
         this.difficultyMode = difficultyMode;
+        this.isFinished = false;
+    }
+
+    public void setFinishedDate(LocalDateTime finishedDate) {
+        this.finishedDate = finishedDate;
+    }
+
+    public LocalDateTime getFinishedDate() {
+        return finishedDate;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
     }
 
     /**
